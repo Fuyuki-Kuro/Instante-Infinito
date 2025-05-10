23516fec4081b9ce2e863c44e3dd2f725f8ff321
Fuyuki-Kuro <fuyuki.g21@gmail.com>
2025-05-09 22:29:11 -0300

    Correção JWT

--- auth/auth.py
+++ auth/auth.py
@@ -11,7 +11,13 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 def gerar_token(user_id: int):
     payload = {
-        "sub": str(user_id),  # ou "user_id"
+        "sub": str(user_id),
         "exp": datetime.now(timezone.utc) + timedelta(days=3650)
     }
-    return jwt.encode(payload, SECRET_KEY, algorithm="HS256")
+    token = jwt.encode(payload, SECRET_KEY, algorithm="HS256")
+    
+    # Converter para string se for bytes
+    if isinstance(token, bytes):
+        token = token.decode("utf-8")
+
+    return token
